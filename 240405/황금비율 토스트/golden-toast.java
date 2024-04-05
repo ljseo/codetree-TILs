ecd068893c5bee07b9ead2209f316dcfe19f1959
이준서 <108654626+quasiwest@users.noreply.github.com>
2024-04-05 16:20:22 +0900

    Title: 황금비율 토스트, Time: 3027ms, Memory: 31MB, Status: Time Limit Exceed - CodeTree

---
+++ 240405/황금비율 토스트/golden-toast.java
@@ -0,0 +1,55 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n,m; //식빵의 개수, 레시피 암호문의 개수
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        char[] str = br.readLine().toCharArray();
+        List<Character> list =  new LinkedList<>();
+
+        for (int i = 0; i < str.length; i++) {
+            list.add(str[i]);
+        }
+
+        ListIterator<Character> iter = list.listIterator(list.size());
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            String c = st.nextToken();
+            if(c.equals("L")){
+                if(iter.hasPrevious()){
+                    iter.previous();
+                }
+            }
+            else if(c.equals("R")){
+                if(iter.hasNext()){
+                    iter.next();
+                }
+            }
+            else if(c.equals("P")){
+                iter.add(st.nextToken().toCharArray()[0]);
+            }
+            else{
+                if(iter.hasNext()){
+                    iter.next();
+                    iter.remove();
+                }
+            }
+//            System.out.println(list.toString());
+        }
+        for (int i = 0; i < list.size(); i++) {
+            System.out.printf(String.valueOf(list.get(i)));
+        }
+    }
+}
