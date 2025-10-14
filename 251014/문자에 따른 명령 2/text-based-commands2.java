0b0d02809c581c0af765a407b5883ed34db63ec7
이준서 <108654626+ljseo@users.noreply.github.com>
2025-10-14 23:42:24 +0900

    Title: 문자에 따른 명령 2, Time: 171ms, Memory: 11MB, Status: Passed - Codetree

--- 251014/문자에 따른 명령 2/text-based-commands2.java
+++ 251014/문자에 따른 명령 2/text-based-commands2.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     static int[] dx = {0,1,0,-1};
-    static int[] dy = {1,0,-1, 0};
+    static int[] dy = {1,0,-1,0};
     
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
