06ccbe107cad3a7914e5dcc7cb74b982346568b8
이준서 <108654626+ljseo@users.noreply.github.com>
2025-10-15 13:59:53 +0900

    Title: 작은 구슬의 이동, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 251015/작은 구슬의 이동/small-marble-movement.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int T = sc.nextInt();
+        int R = sc.nextInt();
+        int C = sc.nextInt();
+        String D = sc.next();
+        // Please write your code here.
+
+        int[] dx = new int[]{-1,0,0,1};
+        int[] dy = new int[]{0,1,-1,0};;
+        int nd = getDir(D);
+        while(T-->0){
+            int nx = R + dx[nd];
+            int ny = C + dy[nd];
+            if(inRange(nx, ny, N)){
+                R = nx;
+                C = ny;
+            }
+            else {
+                nd = 3 - nd;
+            }
+        }
+        System.out.print(R +" " + C);
+    }
+    static int getDir(String D){
+        if(D == "U") return 0;
+        else if(D == "R") return 1;
+        else if(D == "L") return 2;
+        else return 3;
+    }
+    static boolean inRange(int x, int y, int n){
+        return 0 <= x && x < n && 0 <= y &&& y < n; 
+    }
+}
