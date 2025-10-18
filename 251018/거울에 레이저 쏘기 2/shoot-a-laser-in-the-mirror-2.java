8f6e3369cf4e85e1a6a6eb1a28f3e7ef13151118
이준서 <108654626+ljseo@users.noreply.github.com>
2025-10-18 22:38:17 +0900

    Title: 거울에 레이저 쏘기 2, Time: 150ms, Memory: 11MB, Status: Passed - Codetree

--- 251018/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.java
+++ 251018/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.java
@@ -27,7 +27,7 @@ public class Main {
                 if(dir % 2 == 0) dir += 1;
                 else dir -= 1;
             }
-            else{
+            else if(grid[x][y] == '\\'){
                 if(dir % 2 == 0){
                     dir = (dir+3)%4;
                 }
@@ -38,6 +38,7 @@ public class Main {
             cnt++;
             x += dx[dir];
             y += dy[dir];
+            // System.out.println(x + " " + y);
         }
         System.out.print(cnt);
     }
@@ -52,22 +53,46 @@ public class Main {
         return (startNum-1) / n;
     }
     static int[] getStartPos(int startNum){
-        if(startNum == 1) return new int[]{0,0};
-        int [] dx = new int[]{0,1,0,-1};
-        int [] dy = new int[]{1,0,-1,0};
-        int x = 0;
-        int y = 0;
-        int dir = 0;
-        for(int i = 2; i<=4*n;i++){
-            if(i % n == 0) {
-                dir = (dir+1) % 4;
-            }
-            else{
-                x += dx[dir];
-                y += dy[dir];
-            }
-            if(i == startNum) return new int[]{x,y};
-        }
-        return new int[]{0,0};
+    int idx = startNum - 1;
+    
+    if(idx < n) {
+        // 위쪽 가장자리: (0, idx)
+        return new int[]{0, idx};
     }
+    else if(idx < 2*n) {
+        // 오른쪽 가장자리: (idx-n, n-1)
+        return new int[]{idx - n, n - 1};
+    }
+    else if(idx < 3*n) {
+        // 아래쪽 가장자리: (n-1, n-1-(idx-2*n))
+        return new int[]{n - 1, n - 1 - (idx - 2*n)};
+    }
+    else {
+        // 왼쪽 가장자리: (n-1-(idx-3*n), 0)
+        return new int[]{n - 1 - (idx - 3*n), 0};
+    }
+}
+    // static int[] getStartPos(int startNum){
+    //     if(startNum == 1) return new int[]{0,0};
+    //     int [] dx = new int[]{0,1,0,-1};
+    //     int [] dy = new int[]{1,0,-1,0};
+    //     int x = 0;
+    //     int y = 1;
+    //     int dir = 0;
+    //     for(int i = 2; i<=4*n;i++){
+    //         if(i % n == 0) {
+    //             dir = (dir+1) % 4;
+    //         }
+    //         else if((i-1) % n == 0) continue;
+    //         else{
+    //             x += dx[dir];
+    //             y += dy[dir];
+    //         }
+    //         if(i == startNum) {
+    //             // System.out.println(i);
+    //             return new int[]{x,y};
+    //         }
+    //     }
+    //     return new int[]{0,0};
+    // }
 }
