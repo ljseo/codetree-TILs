ea7bf71255ae55ba4736a1306f4eadf329911375
이준서 <108654626+ljseo@users.noreply.github.com>
2025-10-18 22:09:47 +0900

    Title: 거울에 레이저 쏘기 2, Time: 384ms, Memory: 18MB, Status: Wrong Answer - Codetree

---
+++ 251018/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.java
@@ -0,0 +1,71 @@
+import java.util.Scanner;
+
+public class Main {
+    static int n;
+    static int[] dx = new int[]{1,0,-1,0};
+    static int[] dy = new int[]{0,-1,0,1};
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        char[][] grid = new char[n][n];
+        for (int i = 0; i < n; i++) {
+            String s = sc.next();
+            for (int j = 0; j < n; j++) {
+                grid[i][j] = s.charAt(j);
+            }
+        }
+        int startNum = sc.nextInt();
+        int dir  = getStartDir(startNum);
+        int[] startPos = getStartPos(startNum);
+        int x = startPos[0];
+        int y = startPos[1];
+        int cnt = 0;
+        while(inRange(x,y)){
+            if(grid[x][y] == '/'){
+                if(dir % 2 == 0) dir += 1;
+                else dir -= 1;
+            }
+            else{
+                if(dir % 2 == 0){
+                    dir = (dir+3)%4;
+                }
+                else{
+                    dir = (dir-3+4) %4;
+                }
+            }
+            cnt++;
+            x += dx[dir];
+            y += dy[dir];
+        }
+        System.out.print(cnt);
+    }
+    static boolean inRange(int x, int y){
+        return 0<=x && x<n && 0<=y && y<n;
+    }
+    static int getStartDir(int startNum){
+        // if(((startNum-1) / n) == 0) return 0;
+        // else if(((startNum-1) / n) == 1) return 1;
+        // else if(((startNum-1) / n) == 2) return 2;
+        // else return 3;
+        return (startNum-1) / n;
+    }
+    static int[] getStartPos(int startNum){
+        if(startNum == 1) return new int[]{0,0};
+        int [] dx = new int[]{0,1,0,-1};
+        int [] dy = new int[]{1,0,-1,0};
+        int x = 0;
+        int y = 0;
+        int cnt = 1;
+        int dir = 0;
+        for(int i = 1; i<=4*n;i++){
+            if(i % n == 0) {
+                dir = (dir+1) % 4;
+            }
+            x += dx[dir];
+            y += dy[dir];
+            cnt++;
+            if(cnt == startNum) return new int[]{x,y};
+        }
+        return new int[]{0,0};
+    }
+}
