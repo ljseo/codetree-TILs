800c3b5a4e84fb9b859acdbfb1cd8fdef72363a5
이준서 <108654626+ljseo@users.noreply.github.com>
2025-10-18 21:00:37 +0900

    Title: 격자 위의 편안한 상태, Time: 124ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 251018/격자 위의 편안한 상태/comfortable-state-on-the-grid.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class Main {
+    static int n;
+    static int[][] map;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        int m = sc.nextInt();
+        map = new int[n+1][n+1];
+        while(m-->0){
+            int r = sc.nextInt();
+            int c = sc.nextInt();
+            map[r][c] = 1;
+            if(isCom(r,c)) System.out.println(1);
+            else System.out.println(0);
+        }
+    }
+    static boolean isCom(int r, int c){
+        int cnt = 0;
+        int [] dr = new int[]{-1,0,1,0};
+        int [] dc = new int[]{0,1,0,-1};
+        for(int i = 0; i<4; i++){
+            int nr = r + dr[i];
+            int nc = c + dc[i];
+            if(inRange(nr,nc) && map[nr][nc] == 1){
+                cnt++;
+            }
+        }
+        return cnt>=3;
+    }
+    static boolean inRange(int r, int c){
+        return 0 <= r && r < n && 0<=c && c < n;
+    }
+}
