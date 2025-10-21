a03441bcae06d37f4c4bc5138812f1398b7417cf
이준서 <108654626+ljseo@users.noreply.github.com>
2025-10-22 00:11:32 +0900

    Title: 체크판위에서 2, Time: 132ms, Memory: 10MB, Status: Passed - Codetree

--- 251022/체크판위에서 2/on-the-checkboard-2.java
+++ 251022/체크판위에서 2/on-the-checkboard-2.java
@@ -12,29 +12,38 @@ public class Main {
         }
         // Please write your code here.
         int cnt = 0;
-        int r = 0;
-        int c = 0;
-        char color = grid[r][c];
         for(int i = 1; i<R; i++){
             for(int j = 1; j<C; j++){
-                if(grid[i][j] != color){
-                    for(int k = i+1; k<R; k++){
-                        for(int l = j+1; l<C; l++){
-                            if(grid[k][l] == color){
-                                for(int x = k+1; x<R; x++){
-                                    for(int y = l+1; y<C; y++){
-                                        if(grid[x][y] != color && x == R-1 && y == C-1){
-                                            cnt++;
-                                        }
-                                    }
-                                }
-                            }
-                        }
+                for(int k = i+1; k<R-1; k++){
+                    for(int l = j+1; l<C-1; l++){
+                        if(grid[0][0] != grid[i][j] &&
+                           grid[i][j] != grid[k][l] &&
+                           grid[k][l] != grid[R-1][C-1]) 
+                           cnt++;
                     }
                 }
             }
         }
         System.out.print(cnt);
-
+        // char color = grid[r][c];
+        // for(int i = 1; i<R; i++){
+        //     for(int j = 1; j<C; j++){
+        //         if(grid[i][j] != color){
+        //             for(int k = i+1; k<R; k++){
+        //                 for(int l = j+1; l<C; l++){
+        //                     if(grid[k][l] == color){
+        //                         for(int x = k+1; x<R; x++){
+        //                             for(int y = l+1; y<C; y++){
+        //                                 if(grid[x][y] != color && x == R-1 && y == C-1){
+        //                                     cnt++;
+        //                                 }
+        //                             }
+        //                         }
+        //                     }
+        //                 }
+        //             }
+        //         }
+        //     }
+        // }
     }
 }
