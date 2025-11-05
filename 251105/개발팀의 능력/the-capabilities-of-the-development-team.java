e568d1bffe48318d782a208c731a259a942e795b
이준서 <108654626+ljseo@users.noreply.github.com>
2025-11-05 22:58:24 +0900

    Title: 개발팀의 능력, Time: 129ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 251105/개발팀의 능력/the-capabilities-of-the-development-team.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class Main {
+    static final int MAX_TOTAL = 3000;
+    static final int MAX_N = 5;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int [] ability = new int[MAX_N];
+        int total = 0;
+        for(int i = 0; i<5; i++){
+            ability[i] = sc.nextInt();
+            total += ability[i];
+        }
+
+        int mn = MAX_TOTAL;
+
+        for(int i = 0; i<MAX_N; i++){
+            for(int j = i + 1; j<MAX_N; j++){
+                for(int k = 0; k < MAX_N; k++){
+                    for(int l = k + 1; l<MAX_N; l++){
+                        if(i == k || i == l || j== k || j == l) continue;
+                        int first = ability[i] + ability[j];
+                        int second = ability[j] + ability[l];
+                        int third = total - first - second;
+                        if(first == second || second == third || third == first) continue;
+                        mn = Math.min(mn, Math.max(first, Math.max(second, third)) - Math.min(first,Math.min(second, third)));
+                    }
+                }
+            }
+        }
+        System.out.println(mn);
+
+    }
+}
