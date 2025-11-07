d854efbb7f5ad0a83a5f3c9041ba1e632475cdf3
이준서 <108654626+ljseo@users.noreply.github.com>
2025-11-07 11:39:09 +0900

    Title: 삼각형 만들기, Time: 136ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 251107/삼각형 만들기/create-triangle.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class Main {
+    static int[] x,y;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        x = new int[n];
+        y = new int[n];
+        for (int i = 0; i < n; i++) {
+            x[i] = sc.nextInt();
+            y[i] = sc.nextInt();
+        }
+
+        int area = 0;
+        for(int i = 0; i<n; i++){
+            for(int j = i+1; j<n; j++){
+                for(int k = j + 1; k<n; k++){
+                    if(x[i] == x[j] || x[i] == x[k] || x[j] == x[k] 
+                    && y[i] == y[j] || y[i] == y[k] || y[j] == y[k]){
+                        area = Math.max(area, getArea(i,j,k));
+                    }
+                }
+            }
+        }
+        System.out.println(area);
+    }
+    static int getArea(int a, int b, int c){
+        int first = (x[b] - x[a]) * (y[c] - y[a]);
+        int second = (x[c] - x[a]) * (y[b] - y[a]);
+
+        return Math.abs(first - second);
+    }
+}
