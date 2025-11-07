44461af9c627f186a9dfd9648a6ec07c45c5eb7f
이준서 <108654626+ljseo@users.noreply.github.com>
2025-11-07 11:53:14 +0900

    Title: 삼각형 만들기, Time: 171ms, Memory: 11MB, Status: Passed - Codetree

--- 251107/삼각형 만들기/create-triangle.java
+++ 251107/삼각형 만들기/create-triangle.java
@@ -15,9 +15,14 @@ public class Main {
         for(int i = 0; i<n; i++){
             for(int j = i+1; j<n; j++){
                 for(int k = j + 1; k<n; k++){
-                    if(x[i] == x[j] || x[i] == x[k] || x[j] == x[k] 
-                    && y[i] == y[j] || y[i] == y[k] || y[j] == y[k]){
-                        area = Math.max(area, getArea(i,j,k));
+                    if((x[i] == x[j] || x[i] == x[k] || x[j] == x[k]) 
+                    && (y[i] == y[j] || y[i] == y[k] || y[j] == y[k])){
+                        int getA = getArea(i,j,k);
+                        if(getA >= area){
+                            //System.out.println("i: " + i + " j: " + j +" k: " + k +" area: " + getA);
+                            area = getA;
+                        }
+                        //area = Math.max(area, getArea(i,j,k));
                     }
                 }
             }
