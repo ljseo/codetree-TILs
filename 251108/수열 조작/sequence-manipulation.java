5d86980ffd7c4c4e44486b7fe7e50b7dd7076345
이준서 <108654626+ljseo@users.noreply.github.com>
2025-11-08 22:44:17 +0900

    Title: 수열 조작, Time: 175ms, Memory: 17MB, Status: Passed - Codetree

--- 251108/수열 조작/sequence-manipulation.java
+++ 251108/수열 조작/sequence-manipulation.java
@@ -3,16 +3,16 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        Deque<Integer> dq = new ArrayDeque<>();
+        Queue<Integer> dq = new LinkedList<>();
         
         for(int i = 1; i<=n; i++){
-            dq.addLast(i);
+            dq.add(i);
         }
         while(dq.size() != 1){
-            dq.pollFirst();
-            dq.addLast(dq.pollFirst());
+            dq.poll();
+            dq.add(dq.poll());
         }
-        System.out.println(dq.pollFirst());
+        System.out.println(dq.poll());
 
     }
 }
