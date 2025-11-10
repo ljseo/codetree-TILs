d955fd9b37238995256f22f89e75feade682443e
이준서 <108654626+ljseo@users.noreply.github.com>
2025-11-11 00:32:10 +0900

    Title: 기수 정렬 구현, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 251111/기수 정렬 구현/implement-radix-sort.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+public class Main {
+    static final int MAX_VALUE = 100_000;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
+
+        List[] lists = new List[10];
+
+        for(int i = 0; i<10; i++){
+            lists[i] = new ArrayList<>();
+        }
+
+        int len = Integer.toString(MAX_VALUE).length();
+        for(int i = 0; i<len; i++){
+            for(int j = 0; j<n; j++){
+                int idx = Integer.toString(arr[j]).length() - i - 1;
+                int val = 0;
+                if(idx < 0) continue;
+                lists[Integer.toString(arr[j]).charAt(idx) - '0'].add(arr[j]);
+            }
+
+            int[] nextArr = new int[n];
+            int idx = 0;
+            for(int j = 0; j<10; j++){
+                for(int a : lists[j]){
+                    nextArr[idx++] = a;
+                }
+            }
+            arr = nextArr;
+        }
+        for(int a : arr){
+            System.out.print(a + " ");
+        }
+        
+            
+
+
+    }
+}
