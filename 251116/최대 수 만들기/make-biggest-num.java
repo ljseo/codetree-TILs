92e5e4c8beb8f66d9f73ee0a498f2aa5bd255030
이준서 <108654626+ljseo@users.noreply.github.com>
2025-11-16 01:12:38 +0900

    Title: 최대 수 만들기, Time: 262ms, Memory: 15MB, Status: Wrong Answer - Codetree

---
+++ 251116/최대 수 만들기/make-biggest-num.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        Integer[] arr = new Integer[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        Arrays.sort(arr, (o1,o2) -> {
+            String a = Integer.toString(o1);
+            String b = Integer.toString(o2);
+            if(a.contains(b) || b.contains(a)){
+                String ab = a+b;
+                String ba = b+a;
+                if(ab.compareTo(ba) < 0) return o1-o2;
+                else return o2 - o1;
+            }
+            if(a.compareTo(b) < 0) return o1-o2;
+            return o2-o1;
+        });
+        StringBuilder sb = new StringBuilder();
+        for(int i = arr.length-1; i>=0; i--){
+            sb.append(arr[i]);
+        }
+        System.out.print(sb);
+        
+    }
+}
