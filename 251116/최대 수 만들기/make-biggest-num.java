93dce27d3d98e38bead9a00bd2eeb8d5a86c89d5
이준서 <108654626+ljseo@users.noreply.github.com>
2025-11-16 01:17:03 +0900

    Title: 최대 수 만들기, Time: 1146ms, Memory: 37MB, Status: Passed - Codetree

--- 251116/최대 수 만들기/make-biggest-num.java
+++ 251116/최대 수 만들기/make-biggest-num.java
@@ -14,11 +14,13 @@ public class Main {
             if(a.contains(b) || b.contains(a)){
                 String ab = a+b;
                 String ba = b+a;
-                if(ab.compareTo(ba) < 0) return o1-o2;
-                else return o2 - o1;
+                return ab.compareTo(ba); 
+                // if(ab.compareTo(ba) < 0) return o2-o1;
+                // else return o1 - o2;
             }
-            if(a.compareTo(b) < 0) return o1-o2;
-            return o2-o1;
+            return a.compareTo(b); 
+            // if(a.compareTo(b) < 0) return o1-o2;
+            // return o2-o1;
         });
         StringBuilder sb = new StringBuilder();
         for(int i = arr.length-1; i>=0; i--){
