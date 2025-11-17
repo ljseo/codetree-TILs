7bf468807f6b6303749cb3e4839d45283b7a5826
이준서 <108654626+ljseo@users.noreply.github.com>
2025-11-17 23:19:45 +0900

    Title: 특정 문자로 시작하는 문자열, Time: 129ms, Memory: 10MB, Status: Passed - Codetree

--- 251117/특정 문자로 시작하는 문자열/strings-that-start-with-a-specific-character.java
+++ 251117/특정 문자로 시작하는 문자열/strings-that-start-with-a-specific-character.java
@@ -2,8 +2,8 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String[] str = new String[n];
         int n = sc.nextInt();
+        String[] str = new String[n];
         for(int i = 0; i<n; i++){
             str[i] = sc.next();
         }
