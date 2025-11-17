01e65487ca493b1473bc0ff69f84698def3ca90a
이준서 <108654626+ljseo@users.noreply.github.com>
2025-11-17 23:11:44 +0900

    Title: 단어로 구분하기 3, Time: 126ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251117/단어로 구분하기 3/separate-words-with-words-3.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] str = new String[10];
+        for(int i = 0; i < 10; i++){
+            str[i] = sc.next();
+        }
+        for(int i = str.length-1; i >= 0; i--){
+            System.out.println(str[i]);
+        }
+    }
+}
