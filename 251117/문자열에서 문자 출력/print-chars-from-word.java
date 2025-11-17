bb928c9606546038bed5024a6811efd9032c46ef
이준서 <108654626+ljseo@users.noreply.github.com>
2025-11-17 23:21:03 +0900

    Title: 문자열에서 문자 출력, Time: 117ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251117/문자열에서 문자 출력/print-chars-from-word.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        for(char c : str.toCharArray()){
+            System.out.println(c);
+        }
+    }
+}
