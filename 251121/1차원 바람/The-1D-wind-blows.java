cdec342a98eaea646f5cfa9b109285e75c928e39
이준서 <108654626+ljseo@users.noreply.github.com>
2025-11-21 03:41:53 +0900

    Title: 1차원 바람, Time: 433ms, Memory: 23MB, Status: Passed - Codetree

--- 251121/1차원 바람/The-1D-wind-blows.java
+++ 251121/1차원 바람/The-1D-wind-blows.java
@@ -1,30 +1,69 @@
 import java.util.Scanner;
 
 public class Main {
+
+    static final int SHIFT_LEFT = 1;
+    static final int SHIFT_RIGHT = 0;
+    
     static int n,m,q;
     static int[][] a;
 
-    static boolean isPos(int r1, int r2){
-        for(int i = 0; i < m; i++){
-            if(a[r1][i] == a[r2][i]) return true;
+    static void shift(int row, int dir){
+        if(dir == SHIFT_RIGHT){
+            int temp = a[row][m-1];
+            for(int i = m-1; i > 0; i--){
+                a[row][i] = a[row][i-1];
+            }
+            a[row][0] = temp;
         }
+
+        else{
+            int temp = a[row][0];
+            for(int i = 0; i < m-1; i++){
+                a[row][i] = a[row][i+1];
+            }
+            a[row][m-1] = temp;
+        }
+    }
+
+    public static boolean hasSameNumber(int row1, int row2) {
+        for(int col = 0; col < m; col++)
+            if(a[row1][col] == a[row2][col])
+                return true;
+        
         return false;
     }
 
-    static void leftShift(int r){
-        int temp = a[r][m-1];
-        for(int i = m-1; i > 0; i--){
-            a[r][i] = a[r][i-1];
-        }
-        a[r][0] = temp;
+    static int flip(int dir){
+        return dir == SHIFT_LEFT ? SHIFT_RIGHT : SHIFT_LEFT;
     }
 
-    static void rightShift(int r){
-        int temp = a[r][0];
-        for(int i = 0; i < m-1; i++){
-            a[r][i] = a[r][i+1];
+    static void simulate(int startRow, int startDir){
+
+        shift(startRow, startDir);
+        startDir = flip(startDir);
+        
+        int dir = startDir;
+        for(int row = startRow; row > 0; row--){
+            if(hasSameNumber(row, row-1)){
+                shift(row-1, dir);
+                dir = flip(dir);
+            }
+            else {
+                break;
+            }
         }
-        a[r][m-1] = temp;
+        dir = startDir;
+        for(int row = startRow; row < n-1; row++){
+            if(hasSameNumber(row,row+1)){
+                shift(row+1, dir);
+                dir = flip(dir);
+            }
+            else {
+                break;
+            }
+        }
+
     }
     
     public static void main(String[] args) {
@@ -36,44 +75,14 @@ public class Main {
         for (int i = 0; i < n; i++)
             for (int j = 0; j < m; j++)
                 a[i][j] = sc.nextInt();
-        for (int t = 0; t < q; t++) {
+        while(q-->0){
             int r = sc.nextInt() - 1;
             char d = sc.next().charAt(0);
-            char di = d;
-            if(d == 'L') {
-                leftShift(r);
-                d = 'R';
-                di = d;
-            }
-            else {
-                rightShift(r);
-                d = 'L';
-                di = d;
-            }
-            for(int i = r-1; i>=0; i--){
-                if(!isPos(i,i+1)) break;
-                if(d == 'L') {
-                    leftShift(i);
-                    d = 'R';
-                }
-                else {
-                    rightShift(i);
-                    d = 'L';
-                }
-            }
-            d = di;
-            for(int i = r+1; i<n; i++){
-                if(!isPos(i,i-1)) break;
-                if(d == 'L') {
-                    leftShift(i);
-                    d = 'R';
-                }
-                else {
-                    rightShift(i);
-                    d = 'L';
-                }
-            }
+            
+            simulate(r, d == 'L' ? SHIFT_RIGHT : SHIFT_LEFT);
+
         }
+
         for(int [] arr : a){
             for(int v : arr){
                 System.out.print(v + " ");
