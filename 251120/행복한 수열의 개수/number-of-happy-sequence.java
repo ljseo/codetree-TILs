1c11779edae795abcec90ed0093d450f57ec75d7
이준서 <108654626+ljseo@users.noreply.github.com>
2025-11-20 19:42:28 +0900

    Title: 행복한 수열의 개수, Time: 124ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 251120/행복한 수열의 개수/number-of-happy-sequence.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+  
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[][] grid = new int[n][n];
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                grid[i][j] = sc.nextInt();
+
+        int ans = 0;
+        for(int i = 0; i < n; i++){
+            int num = grid[i][0];
+            int seq = 1;
+            int mxSeq = -1;
+            for(int j = 1; j < n; j++){
+                int num2 = grid[i][j];
+                if(num == num2){
+                    seq++;
+                }
+                else{
+                    seq = 1;
+                }
+                mxSeq = Math.max(mxSeq, seq);
+                num = num2;
+            }
+            if(mxSeq >= m){
+                ans++;
+            }
+        }
+        for(int i = 0; i < n; i++){
+            int num = grid[0][i];
+            int seq = 1;
+            int mxSeq = -1;
+            for(int j = 1; j < n; j++){
+                int num2 = grid[j][i];
+                if(num == num2){
+                    seq++;
+                }
+                else{
+                    seq = 1;
+                }
+                mxSeq = Math.max(mxSeq, seq);
+                num = num2;
+            }
+            if(mxSeq >= m) ans++;
+        }
+        System.out.print(ans);
+
+    }
+}
