3ff939c2d0a88a100b9a276a63fc828202325ca8
이준서 <108654626+ljseo@users.noreply.github.com>
2025-11-23 20:37:54 +0900

    Title: K번 최댓값으로 이동하기, Time: 127ms, Memory: 10MB, Status: Passed - Codetree

--- 251123/K번 최댓값으로 이동하기/move-to-max-k-times.java
+++ 251123/K번 최댓값으로 이동하기/move-to-max-k-times.java
@@ -17,7 +17,12 @@ public class Main {
     static int startRow,startCol;
 
     static int[][] grid =  new int[MAX_N][MAX_N];
+    static boolean[][] visited = new boolean[MAX_N][MAX_N];
+
     static Deque<Point> queue = new ArrayDeque<>();
+
+    static int[] dr = {-1, 1, 0, 0};
+    static int[] dc = { 0, 0,-1, 1};
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -31,37 +36,72 @@ public class Main {
         startRow = sc.nextInt() - 1;
         startCol = sc.nextInt() - 1;
 
-        queue.add(new Point(startRow, startCol));
+        
+        while(k-->0){
+            bfs(startRow, startCol);
+        }
+        startRow++;
+        startCol++;
+        System.out.println(startRow + " " + startCol);
+        
+    }
 
-
-
-        while(k-->0 && !queue.isEmpty()){
-
-            Point currentPoint = queue.poll();
+    static void bfs(int row, int col){
+        
+        
+        for(int i = 0; i < n; i++){
+            Arrays.fill(visited[i], false);
+        }
+        queue.add(new Point(row, col));
+        visited[row][col] = true;
+        int standValue = grid[row][col];
+        int mxRow = -1;
+        int mxCol = -1;
+        int mx = -1;
+        while(!queue.isEmpty()){
             
-            int nr = -1;
-            int nc = -1;
-            int mx = 0;
-            for(int i = 0; i < n; i++){
-                for(int j = 0; j < n; j++){
-                    if(grid[i][j] < grid[currentPoint.row][currentPoint.col]){
-                        if(grid[i][j] > mx){
-                            mx = grid[i][j];
-                            nr = i;
-                            nc = j;
+            Point currentPoint = queue.poll();
+
+            for(int i = 0; i < 4; i++){
+                int nr = currentPoint.row + dr[i];
+                int nc = currentPoint.col + dc[i];
+
+                if(isValid(nr, nc, standValue)){
+                    if(grid[nr][nc] > mx){
+                        mx = grid[nr][nc];
+                        mxRow = nr;
+                        mxCol = nc;
+                    }
+                    else if(grid[nr][nc] == mx){
+                        if(nr < mxRow){
+                            mxRow = nr;
+                            mxCol = nc;
+                        }
+                        else if(nr == mxRow && nc < mxCol){
+                            mxCol = nc;
                         }
                     }
+                    queue.add(new Point(nr, nc));
+                    visited[nr][nc] = true;
                 }
             }
-            if(nr == -1 && nc == -1){
-                System.out.println(currentPoint.row + " " + currentPoint.col);
-            }
-            queue.add(new Point(nr, nc));
         }
-        if(!queue.isEmpty()){
-            Point p = queue.poll();
-            System.out.println(p.row + " " + p.col);
+        if(mxRow != -1 && mxCol != -1){
+            startRow = mxRow;
+            startCol = mxCol;
         }
+        
+    }
+    
+    static boolean isValid(int row, int col, int value){
 
+        if(!inRange(row, col)) return false;
+        if(visited[row][col]) return false;
+        if(value <= grid[row][col]) return false;
+        return true;
+    }
+
+    static boolean inRange(int row, int col){
+        return 0 <= row && row < n && 0 <= col && col < n;
     }
 }
